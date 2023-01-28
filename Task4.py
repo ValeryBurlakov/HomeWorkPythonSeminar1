b872a1024b21916a1580fc070a47dfd8ee1852ac
ValeryBurlakov <wogl@mail.ru>
2023-01-29 00:26:31 +0300

    added Task4.py

---
+++ Task4.py
@@ -0,0 +1,8 @@
+print("Подсчёт количества журавликов. Задача 4:")
+print("Введите общее количество журавликов: ")
+S = int(input())
+
+Peter = S / S
+Sergey = S / S
+Katia = (Peter + Sergey) * 2
+print(f"Катя сделала: {int(Katia)} \nПетя сделал: {int(Peter)} \nСергей сделал: {int(Sergey)}")
